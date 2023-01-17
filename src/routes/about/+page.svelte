822d77640d71f1a2e8f737af81ede4dbba8ef102
Ralph <blackgandalf83@gmail.com>
2023-01-17 16:21:52 -0500

    started footer

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,11 +1,22 @@
 <script>
+	// Components
+	import HeadTags from '$lib/components/head-tags/HeadTags.svelte';
 	import TwoCol from '$lib/components/two-col/TwoCol.svelte'
+
+	// Start: Local component properties
+	const metaData = {
+		title: `About Good Food`,
+		description:
+			'Sveltekit starter project created with sveltekit, typescript, tailwindcss, postcss, husky, and storybook. The project has the structure set up for the scaleable project. (sveltekit, typescript, tailwindcss, postcss, husky, Storybook).',
+		keywords: ['about', 'about good food', 'good food story'],
+		content : 'About Good Food'
+	};
 </script>
 
-<svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
-</svelte:head>
+<!-- Start: Header Tag -->
+<HeadTags metaData="{metaData}" />
+<!-- End: Header Tag -->
+
 
 <TwoCol 
 	title='About' 
